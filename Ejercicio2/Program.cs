ccd4195db02686f600c8682255bfd4079ea1fa34
gabriellongom <gabriellongombado@gmail.com>
2022-09-15 17:41:33 -0300

    ejercicio 2

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -6,6 +6,26 @@ using System.Threading.Tasks;
 
 namespace Ejercicio2
 {
+    public class Persona
+    {
+
+        private static char Sexo_Default = 'H';
+        private static char Peso_Ideal = 'H';
+        private static char Gordo = 'H';
+        private static char Flaco = 'H';
+
+        private string nombre = "";
+        private int edad = 0;
+        private int DNIgenerado;
+        private char sexo;
+        private float peso = 0;
+        private float altura = 0;
+
+        public Persona()
+        {
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
