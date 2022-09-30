e5c9528037e3b10fd4ea8d3adbfebf0af9b21d20
Agus-Torres <agustoplasencia2006@gmail.com>
2022-09-30 17:41:00 -0300

    Ejercicio 5 y ejercicio 6

--- Ejercicio5/Program.cs
+++ Ejercicio5/Program.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace Ejercicio5
 {
+    public class Serie
+    {
+        private string titulo;
+        private int numTemporadas = 3;
+        private bool entregado = false;
+        private string genero;
+        private string creador;
+
+        public Serie()
+        {
+
+        }
+    }
+    public class Videojuego
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
--- Ejercicio6/Program.cs
+++ Ejercicio6/Program.cs
@@ -6,10 +6,88 @@ using System.Threading.Tasks;
 
 namespace Ejercicio6
 {
-    class Program
+    public class Libro
     {
-        static void Main(string[] args)
+
+        private int ISBN;
+        private string titulo;
+        private string autor;
+        private int numPaginas;
+
+        public Libro(int ISBN, string Titulo, string Autor, int NumPaginas)
         {
+
+            this.ISBN = ISBN;
+            this.titulo = Titulo;
+            this.autor = Autor;
+            this.numPaginas = NumPaginas;
+
+        }
+
+        public int isbn
+        {
+            get
+            {
+                return ISBN;
+            }
+            set
+            {
+                ISBN = value;
+            }
+        }
+        public string Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+            set
+            {
+                titulo = value;
+            }
+        }
+        public string Autor
+        {
+            get
+            {
+                return autor;
+            }
+            set
+            {
+                autor = value;
+            }
+        }
+        public int NumPaginas
+        {
+            get
+            {
+                return numPaginas;
+            }
+            set
+            {
+                numPaginas = value;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Libro libro1 = new Libro(1423456, "Libro1", "Autor1", 516);
+                Libro libro2 = new Libro(5985458, "Libro1LaSecuela", "Autor2", 2);
+
+                Console.WriteLine("El libro " + libro1.Titulo + " con ISBN " + libro1.isbn + " creado por el autor " + libro1.Autor + " tiene " + libro1.NumPaginas + " páginas");
+                Console.WriteLine("El libro " + libro2.Titulo + " con ISBN " + libro2.isbn + " creado por el autor " + libro2.Autor + " tiene " + libro2.NumPaginas + " páginas");
+
+                if (libro1.NumPaginas > libro2.NumPaginas)
+                {
+                    Console.WriteLine("El libro " + libro1.titulo + "es el que tiene mas paginas");
+                }
+                else
+                {
+                    Console.WriteLine("El libro " + libro2.titulo + "es el que tiene mas paginas");
+                }
+            }
         }
     }
 }
