f23a69933426a5d435df7eef9d8a7deabd4eef09
gabriellongombardo <gabriellongombardo@gmail.com>
2022-11-03 03:01:38 -0300

    Ejercicio 8

---
+++ Ejercicio8/Personas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    internal abstract class Personas
+    {
+        private string nombre;
+        private int edad;
+        private char sexo;
+        private bool asistio;
+
+        public Personas(string nombre, int edad, char sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public char Sexo
+        {
+            get
+            {
+                return sexo;
+            }
+            set
+            {
+                sexo = value;
+            }
+        }
+
+        public bool Asistio
+        {
+            get
+            {
+                return asistio;
+            }
+        }
+
+        public 
+    }
+
+
+}
