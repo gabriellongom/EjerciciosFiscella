a13564eda1c559e8b51149d456b276192121a146
gabriellongom <gabriellongombardo@gmail.com>
2022-11-10 17:47:27 -0300

    Comienzo Ejercicio 10

--- Ejercicio10/Baraja.cs
+++ Ejercicio10/Baraja.cs
@@ -8,5 +8,41 @@ namespace Ejercicio10
 {
     class Baraja
     {
+        Carta[] mazo;
+        
+        public Baraja()
+        {
+
+        }
+
+        public void Baraja()
+        {
+
+        }
+
+        public Carta SiguienteCarta()
+        {
+
+        }
+
+        public int CartasDisponibles()
+        {
+
+        }
+
+        public Carta DarCartas(int Num)
+        {
+
+        }
+
+        public Carta CartasMonton()
+        {
+
+        }
+
+        public Carta MostrarBaraja()
+        {
+
+        }
     }
 }
--- Ejercicio10/Carta.cs
+++ Ejercicio10/Carta.cs
@@ -8,10 +8,12 @@ namespace Ejercicio10
 {
     class Carta
     {
-        private static string[] palos = {}
+        private static string[] palos = { "Oros", "Copas", "Bastos", "Espadas" };
+        private static int[] valores = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12 };
 
         private string palo;
         private int valor;
+        private bool enMazo;
 
     }
 }
